9e7de07f2e4cf554b65bfc3d2c70fd50cf2a9c07
Gui Ven Var <guivenvar@gmail.com>
2024-05-01 18:57:49 +0200

    Modifications at Manifest and js
    Se modifica el manifest segun las reglas de Chrome y se modifica el JS debido a un error en las búsquedas de google, debido a que el scroll se solapaba en todas las páginas.

--- script.js
+++ script.js
@@ -5,28 +5,33 @@ console.log("%cADEventExtension START", "color: green; font-size: 64px;");
 //Iniciamos la primera vez sin setTimeout dado que la ejecución del mismo tiene un delay que hace que no se vea totalmente fluido
 if (document.querySelector("div.fc-ab-root")) { // Válido para Forocoches, Marca y Elmundo
   document.querySelector("div.fc-ab-root").remove();
+  document.body.className = ""; // Eliminar todas las clases del body
+  document.body.style.overflow = "scroll";  
   console.log("%cMODAL DELETED", "color: red; font-size: 64px;");
 }
 else if (document.querySelector("#didomi-host")) { // Valido para Elmundo y Marca en modo incognito
   document.querySelector("#didomi-host").remove();
+  document.body.className = ""; // Eliminar todas las clases del body
+  document.body.style.overflow = "scroll";
   console.log("%cMODAL DELETED", "color: red; font-size: 64px;");
 }
-document.body.className = ""; // Eliminar todas las clases del body
-document.body.style.overflow = "scroll";
+
 
 
 // Definir una función para ejecutar tu código
 function executeCode() {
   if (document.querySelector("div.fc-ab-root")) { // Válido para Forocoches, Marca y Elmundo
-    document.querySelector("div.fc-ab-root").remove();
+    document.querySelector("div.fc-ab-root").remove();  
+    document.body.className = ""; // Eliminar todas las clases del body
+    document.body.style.overflow = "scroll"; 
     console.log("%cMODAL DELETED", "color: red; font-size: 64px;");
   }
   else if (document.querySelector("#didomi-host")) { // Valido para Elmundo y Marca en modo incognito
     document.querySelector("#didomi-host").remove();
+    document.body.className = ""; // Eliminar todas las clases del body
+    document.body.style.overflow = "scroll"; 
     console.log("%cMODAL DELETED", "color: red; font-size: 64px;");
   }
-  document.body.className = ""; // Eliminar todas las clases del body
-  document.body.style.overflow = "scroll";
 }
 
 // Ejecutar el código repetidamente .5
